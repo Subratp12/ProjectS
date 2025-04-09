7f3ffa4ec8ce699790675267c7e8b10ca9ae93e9
unknown <subrat.padhi1204@outlook.com>
2025-04-09 16:47:33 +0530

    SFSL,changed the AMI-ID in windows merged at 16:47

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ ec2 = boto3.client('ec2')
 
 # Launch an EC2 instance
 response = ec2.run_instances(
+<<<<<<< HEAD
     ImageId='ami-11111111111',  # Amazon Linux 2 AMI (Free tier eligible, update as needed)
     InstanceType='t3.micro',
     KeyName='SFSL',  # Ensure this key pair exists in your AWS account
